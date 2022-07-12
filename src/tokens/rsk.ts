1727619bc143f0c2382de91856bf773fddb0d88e
Cryptoiner <daniel@thinkanddev.com>
2022-07-12 13:49:39 -0500

    RSK assets

---
+++ src/tokens/rsk.ts
@@ -0,0 +1,29 @@
+import { ChainId } from '../chainIds';
+import { IToken } from '../interfaces';
+
+export const tokens: IToken[] = [
+  {
+    name: 'RBTC',
+    address: '0x0000000000000000000000000000000000000000',
+    chainId: ChainId.RSKTestnet,
+    enabled: true,
+    decimals: 18,
+    symbol: 'RBTC',
+  },
+  {
+    name: 'USDT',
+    address: '0xEf213441a85DF4d7acBdAe0Cf78004E1e486BB96',
+    chainId: ChainId.RSKTestnet,
+    enabled: true,
+    decimals: 18,
+    symbol: 'USDT',
+  },
+  {
+    name: 'Dai Token',
+    address: '0x6B1a73d547F4009A26B8485b63D7015D248AD406',
+    chainId: ChainId.RSKTestnet,
+    enabled: true,
+    decimals: 18,
+    symbol: 'DAI',
+  },
+];

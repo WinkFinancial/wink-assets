42277c4379c6f9377e805f039977e9ec0560e8ab
Cryptoiner <daniel@thinkanddev.com>
2022-07-12 16:34:48 -0500

    Updated RSK chainID

--- src/networks/index.ts
+++ src/networks/index.ts
@@ -69,7 +69,7 @@ export const networksByChainId: INetworkByChainId = {
     isMainnet: false,
   },
   [ChainId.RSK]: {
-    name: 'RSK EVM Mainnet',
+    name: 'RSK Mainnet',
     chainId: ChainId.RSK,
     payrollAddress: '0x0000000000000000000000000000000000000000',
     routerAddress: '0xf55c496bB1058690DB1401c4B9C19f3f44374961', // rskswap v2
@@ -80,7 +80,7 @@ export const networksByChainId: INetworkByChainId = {
     isMainnet: true,
   },
   [ChainId.RSKTestnet]: {
-    name: 'RSK EVM Testnet',
+    name: 'RSK Testnet',
     chainId: ChainId.RSKTestnet,
     payrollAddress: '0x18c42168D834c99E2e7a368a5Fbf39F5BB32e09D',
     routerAddress: '0xf55c496bB1058690DB1401c4B9C19f3f44374961', // rskswap v2
--- src/tokens/rsk.ts
+++ src/tokens/rsk.ts
@@ -5,7 +5,7 @@ export const tokens: IToken[] = [
   {
     name: 'RBTC',
     address: '0x0000000000000000000000000000000000000000',
-    chainId: ChainId.RSKTestnet,
+    chainId: ChainId.RSK,
     enabled: true,
     decimals: 18,
     symbol: 'RBTC',
@@ -13,7 +13,7 @@ export const tokens: IToken[] = [
   {
     name: 'USDT',
     address: '0xEf213441a85DF4d7acBdAe0Cf78004E1e486BB96',
-    chainId: ChainId.RSKTestnet,
+    chainId: ChainId.RSK,
     enabled: true,
     decimals: 18,
     symbol: 'USDT',
@@ -21,7 +21,7 @@ export const tokens: IToken[] = [
   {
     name: 'Dai Token',
     address: '0x6B1a73d547F4009A26B8485b63D7015D248AD406',
-    chainId: ChainId.RSKTestnet,
+    chainId: ChainId.RSK,
     enabled: true,
     decimals: 18,
     symbol: 'DAI',

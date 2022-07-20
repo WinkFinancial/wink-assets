26be392881c15efd0315354353cf2d757477b7c7
Cryptoiner <daniel@thinkanddev.com>
2022-07-20 17:02:29 -0500

    Tron tokens update

--- src/tokens/tron.ts
+++ src/tokens/tron.ts
@@ -4,10 +4,18 @@ import { IToken } from '../interfaces';
 export const tokens: IToken[] = [
   {
     name: 'TRX',
-    address: '0x0000000000000000000000000000000000000000',
+    address: 'T9yD14Nj9j7xAB4dbGeiX9h8unkKHxuWwb',
     chainId: ChainId.Tron,
     enabled: true,
     decimals: 6,
     symbol: 'TRX',
+  },
+  {
+    name: 'USDT',
+    address: 'TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t',
+    chainId: ChainId.Tron,
+    enabled: true,
+    decimals: 6,
+    symbol: 'USDT',
   },
 ];

97e2fedb3a5d0944da28844368512a8b93012159
Pedrow <pmprete@users.noreply.github.com>
2022-08-01 18:14:25 -0300

    Mumbai (#19)
    * Enable mumbai
    
    * Disable MATIC until native cryptocurrency is fixed

--- src/tokens/mumbai.ts
+++ src/tokens/mumbai.ts
@@ -6,7 +6,7 @@ export const tokens: IToken[] = [
     name: 'MATIC',
     address: '0x0000000000000000000000000000000000000000',
     chainId: ChainId.Mumbai,
-    enabled: true,
+    enabled: false,
     decimals: 18,
     symbol: 'MATIC',
     coingeckoId: 'matic-network',

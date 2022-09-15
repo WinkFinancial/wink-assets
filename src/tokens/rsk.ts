d112173a5628b1f7c077d21eae6debd0fcac9d57
Lizandro Zerpa <lizandroal.zerpa@gmail.com>
2022-09-15 10:49:20 -0400

    Added Rootstock native token to RSK (#30)
    * Added Rootstock native token to RSK
    
    * Fix chainId

--- src/tokens/rsk.ts
+++ src/tokens/rsk.ts
@@ -3,6 +3,15 @@ import { IToken } from '../interfaces'
 
 export const tokens: IToken[] = [
   {
+    name: 'rBTC',
+    address: '0x0000000000000000000000000000000000000000',
+    chainId: ChainId.RSK,
+    enabled: false,
+    decimals: 18,
+    symbol: 'RBTC',
+    coingeckoId: 'rootstock',
+  },
+  {
     name: 'RIF Dollar On Chain',
     address: '0x2d919F19D4892381D58edeBeca66D5642Cef1a1f',
     chainId: ChainId.RSK,

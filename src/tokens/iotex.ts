a5abaf115a62f1a40ad3159d63328108c441e803
Pedrow <pmprete@users.noreply.github.com>
2022-10-26 12:11:38 -0300

    Add iotex (#40)
    * Add IoTeX blockchain
    
    * fix weth for testnet
    
    * Add IoTeX mainnet payroll contract

---
+++ src/tokens/iotex.ts
@@ -0,0 +1,55 @@
+import { ChainId } from '../chainIds'
+import { IToken } from '../interfaces'
+
+export const tokens: IToken[] = [
+  {
+    name: 'IoTeX',
+    address: '0x0000000000000000000000000000000000000000',
+    chainId: ChainId.IoTeX,
+    enabled: true,
+    decimals: 18,
+    symbol: 'IOTX',
+    isNativeToken: true,
+    coingeckoId: 'iotex',
+  },
+  {
+    name: 'Crosschain IOTX',
+    address: '0x99B2B0eFb56E62E36960c20cD5ca8eC6ABD5557A',
+    chainId: ChainId.IoTeX,
+    enabled: true,
+    decimals: 18,
+    symbol: 'CIOTX',
+    isNativeToken: false,
+    coingeckoId: 'crosschain-iotx',
+  },
+  {
+    name: 'Tether',
+    address: '0x6fbCdc1169B5130C59E72E51Ed68A84841C98cd1',
+    chainId: ChainId.IoTeX,
+    enabled: true,
+    decimals: 6,
+    symbol: 'USDT',
+    isNativeToken: false,
+    coingeckoId: 'iousdt',
+  },
+  {
+    name: 'USD Coin',
+    address: '0x3B2bf2b523f54C4E454F08Aa286D03115aFF326c',
+    chainId: ChainId.IoTeX,
+    enabled: false,
+    decimals: 6,
+    symbol: 'USDC',
+    isNativeToken: false,
+    coingeckoId: 'iousdc',
+  },
+  {
+    name: 'Binance USD',
+    address: '0xacEE9B11CD4B3f57e58880277aC72c8c41ABe4e4',
+    chainId: ChainId.IoTeX,
+    enabled: false,
+    decimals: 18,
+    symbol: 'BUSD',
+    isNativeToken: false,
+    coingeckoId: 'iobusd',
+  },
+]
